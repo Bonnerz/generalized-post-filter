d4d4acd5add18ea10ebbca3e0044860ed7611c9c
wangwei2009 <373955482ww@gmail.com>
2019-01-23 14:52:47 +0800

    重写代码，用仿真数据实现论文方法

---
+++ experiment1/noise_gen_ULA.m
@@ -0,0 +1,39 @@
+function [ z,P,d] = noise_gen_ULA( input_args )
+%UNTITLED4 此处显示有关此函数的摘要
+%   此处显示详细说明
+%% Initialization
+fs = 16000;                       % Sample frequency
+NFFT = 512;                      % Number of frequency bins (for analysis)
+w = 2*pi*fs*(0:NFFT/2)/NFFT; 
+c = 340;                         % Speed of sound
+L = 2^18;                        % Data length
+r = 0.05;
+[x1,y1,z1]=sph2cart(0,0,r);    % Sensor position 1
+[x2,y2,z2]=sph2cart(0,0,2*r);    % Sensor position 2
+[x3,y3,z3]=sph2cart(0,0,3*r);    % Sensor position 1
+[x4,y4,z4]=sph2cart(0,0,4*r);    % Sensor position 2
+[x5,y5,z5]=sph2cart(0,0,5*r);    % Sensor position 2
+P = [x1 x2 x3 x4 x5;y1 y2 y3 y4 y5;z1 z2 z3 z4 z5]; % Construct position matrix
+M = size(P,2);                           % Number of sensors
+
+% Calculate sensor distances w.r.t. sensor 1
+d = zeros(1,M);
+for m = 2:M
+    d(m) = norm(P(:,m)-P(:,1),2);
+end
+
+%% Generate sensor signals
+params.c = c;
+params.fs = fs;
+
+% 1D example
+params.N_phi = 64;
+z = sinf_1D(d,L,params); 
+
+% 3D example
+% params.N = 256;
+% z = sinf_3D(P,L,params); 
+
+
+end
+
---
+++ experiment1/superdirectiveMVDR_ULA.m
@@ -0,0 +1,96 @@
+function [ DS, x1,H,DI,Fvv] = superdirectiveMVDR_ULA( x,fs,N,frameLength,inc,r,angle,Fvv)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% superdirective beamformer using uniform linear array
+%   
+%      input :
+%          x : input signal ,samples * channel
+%          fs: sample rate
+%          N : fft length,frequency bin number
+%frameLength : frame length,usually same as N
+%        inc : step increment
+%          d : array element spacing
+%      angle : incident angle
+%
+%     output :
+%         DS : delay-sum output
+%         x1 : presteered signal,same size as x
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% N = 256;
+% inc = 32;
+% frameLength = 256;
+c = 340;
+Nele = size(x,2);
+omega = zeros(frameLength,1);
+H = ones(N/2+1,Nele)';
+
+theta = angle*pi/180; %固定一个俯仰角
+
+tao = r*sin(theta)*[0:Nele-1]/c;     %方位角 0 < angle <360
+
+yds = zeros(length(x(:,1)),1);
+x1 = zeros(size(x));
+DI = zeros(N/2+1,1);
+
+M = Nele;
+N_FFT = N;
+f = 0:fs/256:fs/2;
+Fvv = zeros(N_FFT/2+1,M,M);
+for i = 1:M
+    for j = 1:M   
+        if i == j
+            Fvv(:,i,j) = ones(N_FFT/2+1,1);
+        else
+            dij = r*abs(i-j);
+            Fvv(:,i,j) = sin(2*pi*f*dij*1/c)./(2*pi*f*dij*1/c);Fvv(1,i,j) = 0.998;%T(2) = 0.996;
+        end
+    end
+end
+
+
+for k = 1:N/2+1
+%         inv_Fvv = inv(squeeze(Fvv(k,:,:)));
+        omega(k) = 2*pi*k*fs/N;    
+        
+        % 方向向量
+        d = exp(-1j*omega(k)*tao');
+%         H(k,:) = [1;exp(-1j*omega(k)*tao);exp(-1j*omega(k)*2*tao)];
+        %对齐向量，以第一个阵元为参考，
+        %例如若第一个阵元最慢(theata>0),则将第2、3、....个阵元分别延迟exp(-j*w*m*tao)
+%         H(k,:) = [1;exp(-1j*omega(k)*tao);exp(-1j*omega(k)*2*tao);];
+
+        % MVDR soulution
+%         Fvvk = diag(ones(1,Nele));%squeeze(Fvv(k,:,:));
+        Fvv_k = (squeeze(Fvv(k,:,:))+1e-3*eye(Nele));
+        if(1)%k~=31&&k~=20&&k~=16)
+        H(:,k) =    Fvv_k\d ...
+                 ./(d'/Fvv_k*d);
+%        H(:,k) =   1; % DS weights
+        DI(k) = (abs(H(:,k)'*d))^2 ...
+                /(H(:,k)'*squeeze(Fvv(k,:,:))*H(:,k));
+        else
+         H(:,k) = d;
+        end
+end
+for i = 1:inc:length(x(:,1))-frameLength
+
+%     d = zeros(N,Nele);
+%     d(33,:) = 1;
+    d = fft(x(i:i+frameLength-1,:).*hamming(frameLength));
+%     x_fft = dot(H.',d(1:129,:),2);
+    x_fft = H.'.*d(1:N/2+1,:);
+    yf = sum(x_fft,2);
+    Cf = [yf;conj(flipud(yf(2:N/2)))];
+    
+    % 恢复延时累加的信号
+    yds(i:i+frameLength-1) = yds(i:i+frameLength-1)+(ifft(Cf));
+    
+    
+    % 恢复各路对齐后的信号
+    xf  = [x_fft;conj(flipud(x_fft(2:N/2,:)))];
+    x1(i:i+frameLength-1,:) = x1(i:i+frameLength-1,:)+(ifft(xf));
+end
+DS = real(yds);  
+% DI = pow2db(abs(DI));
+
+end
+
